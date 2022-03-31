c9f669e0b9597f2dffb75f4561c3025e5e62b717
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-03-31 12:35:39 +0100

    fixed some bug

--- mentoring_project/mentoring_app/apps.py
+++ mentoring_project/mentoring_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MentoringAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Mentoring_App`'
+    name = 'mentoring_app'
