0978aed16872417c6afed1b2bf293f7b7069b5ed
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-02 11:14:18 +0100

    Moved guidelines page

--- mentoring_project/mentoring_app/urls.py
+++ mentoring_project/mentoring_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.guidelines, name='guidelines'),
+    path('', views.mentor_list, name='mentor_list'),
+    path('', views.ask_for_help, name='ask_for_help')
 ]
--- mentoring_project/mentoring_app/views.py
+++ mentoring_project/mentoring_app/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("EXample")
+    return render(request, 'templates/index.html')
+
+def mentor_list(request):
+    return render(request, 'templates/mentor_list.html')
+
+def guidelines(request):
+    return render(request, "templates/guidelines.html")
+
+def ask_for_help(request):
+    return HttpResponse(request, "templates/ask_for_help.html")
