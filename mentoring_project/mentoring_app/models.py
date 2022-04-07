72e989eec316c5449491358ab7d7bd6becb03347
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-07 11:16:48 +0100

    updated models to reflect design

--- mentoring_project/mentoring_app/models.py
+++ mentoring_project/mentoring_app/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Mentor(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-
-class StudentDetails(models.Model):
     name = models.CharField(max_length=50)
+    subject_area = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
 
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
