cb49742aa1cb948309518219636d4a95a6e76077
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-22 16:07:15 +0100

    idk

--- mentoring_project/mentoring_app/models.py
+++ mentoring_project/mentoring_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Mentor(models.Model):
     name = models.CharField(max_length=50)
     subject_area = models.CharField(max_length=50)
--- mentoring_project/mentoring_app/views.py
+++ mentoring_project/mentoring_app/views.py
@@ -8,20 +8,28 @@ from mentoring_app.models import Mentor
 def index(request):
     return render(request, 'index.html')
 
-def mentor_list(request):
-    return render(request, 'mentor_list.html')
-
 def guidelines(request):
     return render(request, "guidelines.html")
 
 def ask_for_help(request):
-    return render(request, 'ask_for_help.html')
+    return HttpResponse(request, "ask_for_help.html")
 
-class MentorListView(TemplateView):
-    template_path = "templates/mentor_list.html"
+class SubmissionFormView(TemplateView):
+    template_path='templates/ask_for_help.html'
 
-    def get(self,request):
+    def get(self, request):
+        return render(request, self.template_path)
+
+    def post(self, request):
+        form = SubmissionForm(request.POST)
+
+        return render(request, self.template_name)
+
+class MentorList(TemplateView):
+    template_path='templates/mentor_list.html'
+
+    def get(self, request):
         mentors = Mentor.objects.all()
-        
+
         args = {'mentors': mentors}
-        return render(request, self.template_path, args)
+        return render(request, self.template_path, mentors)
