1e4c8f872837d0a254ba1a1fe0db8811096f4b37
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-22 16:20:47 +0100

    appropriate model changes

--- mentoring_project/mentoring_app/models.py
+++ mentoring_project/mentoring_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class Mentor(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    subject_area = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
 
 class Student(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
