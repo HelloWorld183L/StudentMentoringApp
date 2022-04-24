6dcb686b05920faea72db162ca0c969ff08288c1
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-24 22:53:09 +0100

    created mentors list page

--- mentoring_project/mentoring_app/urls.py
+++ mentoring_project/mentoring_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('guidelines', views.guidelines, name='guidelines'),
-    path('mentor_list', views.MentorListView.as_view(), name='mentor_list'),
+    path('mentors/', views.MentorListView.as_view(), name='mentors'),
     path('ask_for_help', views.SubmissionFormView.as_view(), name='ask_for_help')
 ]
--- mentoring_project/mentoring_app/views.py
+++ mentoring_project/mentoring_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
 from mentoring_app.models import Mentor
-
-BASE_TEMPLATE_PATH = 'templates/'
+from django_filters.views import FilterView
+from mentoring_app.filters import MentorFilter
 
 def index(request):
     return render(request, 'index.html')
@@ -12,19 +12,13 @@ def guidelines(request):
     return render(request, "guidelines.html")
 
 class SubmissionFormView(TemplateView):
-    template_path=BASE_TEMPLATE_PATH+'ask_for_help.html'
+    template_path='ask_for_help.html'
 
     def get(self, request):
         return render(request, self.template_path)
 
-class MentorListView(ListView):
-    model = Mentor
-    context_object_name = 'mentor_list'
-    template_path=BASE_TEMPLATE_PATH+'mentors.html'
-    paginate_by = 10
-
-    def get(self, request):
-        mentors = Mentor.objects.all()
-
-        args = {'mentors': mentors}
-        return render(request, self.template_path, mentors)
+class MentorListView(FilterView):
+    template_name="mentors.html"
+    paginate_by=10
+    filterset_class = MentorFilter
+    queryset = Mentor.objects.all()
