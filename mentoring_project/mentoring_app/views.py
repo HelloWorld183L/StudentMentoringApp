a24ed8ff3f58069b4b95cae7aedbff72cfdd4de4
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-24 21:38:17 +0100

    updated mentor list view

--- mentoring_project/mentoring_app/views.py
+++ mentoring_project/mentoring_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from mentoring_app.models import Mentor
 
-# Create your views here.
+BASE_TEMPLATE_PATH = 'templates/'
 
 def index(request):
     return render(request, 'index.html')
@@ -11,21 +11,17 @@ def index(request):
 def guidelines(request):
     return render(request, "guidelines.html")
 
-def ask_for_help(request):
-    return HttpResponse(request, "ask_for_help.html")
-
 class SubmissionFormView(TemplateView):
-    template_path='templates/ask_for_help.html'
+    template_path=BASE_TEMPLATE_PATH+'ask_for_help.html'
 
     def get(self, request):
         return render(request, self.template_path)
 
-    def post(self, request):
-        form = SubmissionForm(request.POST)
-        return render(request, self.template_name)
-
-class MentorListView(TemplateView):
-    template_path='templates/mentor_list.html'
+class MentorListView(ListView):
+    model = Mentor
+    context_object_name = 'mentor_list'
+    template_path=BASE_TEMPLATE_PATH+'mentors.html'
+    paginate_by = 10
 
     def get(self, request):
         mentors = Mentor.objects.all()
