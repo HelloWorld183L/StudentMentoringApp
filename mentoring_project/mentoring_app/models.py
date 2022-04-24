ec24cc072a1301181aa8928540cd2b57c5407063
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-24 13:11:49 +0100

    updated student model + added sample data

--- mentoring_project/mentoring_app/models.py
+++ mentoring_project/mentoring_app/models.py
@@ -9,5 +9,6 @@ class Mentor(models.Model):
 class Student(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
+    subject_area = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
