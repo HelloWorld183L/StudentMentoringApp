867d6074bb1a1b1a1461cdf81ab15987321a544d
Nimenides <nimenides@nimenides-nv4xmbmemz>
2022-04-25 14:26:00 +0100

    ignore

--- mentoring_project/mentoring_app/filters.py
+++ mentoring_project/mentoring_app/filters.py
@@ -4,5 +4,5 @@ from mentoring_app.models import Mentor
 class MentorFilter(FilterSet):
     course = CharFilter(lookup_expr='icontains')
     current_year = NumberFilter(field_name='current_year', lookup_expr='current_year')
-    current_year__gt = NumberFilter(field_name="current_year", lookup_expr="current_year__gt")    
-    current_year__lt = NumberFilter(field_name="current_year", lookup_expr="current_year__lt")
+    current_year__gt = NumberFilter(field_name='current_year', lookup_expr='current_year__gt')    
+    current_year__lt = NumberFilter(field_name='current_year', lookup_expr='current_year__lt')
